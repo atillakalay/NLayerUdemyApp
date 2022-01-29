56751c82d504e49057df924662bc347a3901335c
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-01-29 13:59:48 +0300

    AppDbContext created.

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
