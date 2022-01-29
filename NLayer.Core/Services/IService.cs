e5281d4590bbb95dd0448b35757234d3b129b570
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-01-29 13:44:34 +0300

    IService created.

---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRange(IEnumerable<T> entities);
+    }
+}
