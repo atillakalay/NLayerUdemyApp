add7631ca63883d1cf3dca02c1ea22a7482a7396
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-01-30 12:32:35 +0300

    UnitOfWork created.

---
+++ NLayer.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using NLayer.Core.UnitOfWorks;
+
+namespace NLayer.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
