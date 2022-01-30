ee59263a2eb688fc625a714fd0ca47b6d0a810dc
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-01-30 15:30:14 +0300

    CustomResponseDto created.

---
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public List<String> Errors { get; set; }
+
+        public static CustomResponseDto<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDto<T> { Data = data, StatusCode = statusCode };
+        }
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode };
+        }
+        public static CustomResponseDto<T> Fail(List<String> errors, int statusCode, T data)
+        {
+            return new CustomResponseDto<T> { Data = data, StatusCode = statusCode, Errors = errors };
+        }
+        public static CustomResponseDto<T> Fail(int statusCode, string error)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error } };
+        }
+    }
+}
---
+++ NLayer.Core/DTOs/NoContentDto.cs
@@ -0,0 +1,6 @@
+﻿namespace NLayer.Core.DTOs
+{
+    public class NoContentDto
+    {
+    }
+}
