372a8d524f6e91308aef810cacab08613fc8cc1c
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-02-01 20:32:59 +0300

    ProductsController Delete.

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -74,5 +74,12 @@ namespace NLayer.Web.Controllers
             ViewBag.categories = new SelectList(categoriesDto, "Id", "Name", productDto.CategoryId);
             return View(productDto);
         }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            await _productService.RemoveAsync(product);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
